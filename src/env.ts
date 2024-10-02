aaaf2fee26f4aa9b4e926d9dde4ac77281b08067
lhonRafaat <lhontaekwondo@gmail.com>
2024-10-03 01:55:18 +0300

    feat: update env example and file type

--- src/env.ts
+++ src/env.ts
@@ -9,11 +9,12 @@ const envSchema = z.object({
       z.literal("production"),
     ])
     .default("development"),
-  // ...
+  DB_NAME: z.string(),
+  DB_USER: z.string(),
+  DB_PASS: z.string(),
+  DB_HOST: z.string(),
+  DB_PORT: z.string().default("5432"),
 });
-
-// Validate `process.env` against our schema
-// and return the result
 
 type Env = z.infer<typeof envSchema>;
 
